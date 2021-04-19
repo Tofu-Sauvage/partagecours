8b740d2af3afadce2615a803ffa8214b53f658d7
Charlotte <charlotte_sch@icloud.com>
2021-04-19 12:04:41 +0200

    fixtures

---
+++ src/DataFixtures/ArticlesFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Faker\Factory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ArticlesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i=0; $i<=10; $i++){
+            $article = new Article;
+            $content = '<p>'.join($faker->paragraphs(5), '</p><p>').'<p>';
+            $article->setTitle($faker->sentence())->setContent($content)->setImage($faker->imageUrl())->setCreatedAt(new \DateTime());
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
