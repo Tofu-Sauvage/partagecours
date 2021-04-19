c2647baba373bce45a07a60364476a17f640db9b
Charlotte <charlotte_sch@icloud.com>
2021-04-19 12:24:40 +0200

    boucle for pour les articles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class HomeController extends AbstractController
 {
@@ -19,9 +22,10 @@ class HomeController extends AbstractController
      /**
      * @Route("/articles", name="articles")
      */
-    public function articlesAction(): Response
+    public function articlesAction(ArticleRepository $articleRepository, Request $request): Response
     {
-        return $this->render('home/articles.html.twig');
+        $articles = $articleRepository->findAll();
+        return $this->render('home/articles.html.twig', ['articles'=>$articles]);
     }
     
     /**
